b2b036ddac9e6e2dcaa7e503f92e4fd525f858a6
James Perry <PjPerry28@gmail.com>
2022-10-03 20:58:08 -0700

    finish our skeleton

--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'train'
 require 'city'
-
+require'spec_helper'
 
 describe '#City' do 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,13 @@ require 'city'
 require 'train'
 require 'pry'
 require'spec_helper'
+
+DB = PG.connect({dbname: 'train_system_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM trains *;")
+    DB.exec("DELETE FROM cities *;")
+    DB.exec("DELETE FROM stops *;")
+  end
+end
